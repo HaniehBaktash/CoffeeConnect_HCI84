b223434b96efdb61ccb4bd353237ffec44bcdc41
ChHarding <charding@iastate.edu>
2023-06-06 18:01:54 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 print(" this is my first project")
-my_cats_list=["toffee","tillee","pangool"]
+my_cats_list=["toffee","tillee","pangool", "orange terror"]
 print("I love",my_cats_list[0], "more")
 
 
